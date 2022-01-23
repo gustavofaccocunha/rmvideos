455fa7a5532602596273dbf767e0f38b0587390e
Gustavo Cunha <gustavofaccocunha@hotmail.com>
2022-01-23 15:18:26 -0300

    Aula02-Alteracoes

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -7,6 +7,18 @@ function CadastroCategoria(){
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
 
+            <form>
+                <label>
+                    Nome da Categoria:
+                    <imput type="text"/>
+                </label>
+            
+
+                <button>
+                    Cadastrar
+                </button>
+            </form>    
+
             
 
             <Link to="/">
